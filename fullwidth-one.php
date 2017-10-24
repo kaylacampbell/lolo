d34e12dc8370fdefb22bdfb7189740744f2491f7
kaylacampbell <32884792+kaylacampbell@users.noreply.github.com>
2017-10-23 21:14:51 -0700

    Create fullwidth-one.php

---
+++ fullwidth-one.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Fullwidth One
+*/
+get_header(); ?>
+
+<?php get_template_part('template-part', 'head'); ?>
+
+
+<!-- start content container -->
+<div class="row content">
+
+    <div class="col-md-12 main">
+
+        <?php // theloop
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
+
+        <?php endwhile; ?>
+        <?php else: ?>
+
+            <?php get_404_template(); ?>
+
+        <?php endif; ?>
+
+    </div>
+</div>
+
+<!-- end content container -->
+
+<?php get_footer(one); ?>
